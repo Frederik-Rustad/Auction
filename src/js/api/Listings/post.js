a39c0c2cc9c5c99163ff6af54436ce6cb3ce17ce
Frederik-Rustad <frederik.rustad.89@gmail.com>
2024-03-01 19:12:42 +0100

    post update
    to post multiple image urls to a listing is no longer one large text area where you divide the urls my line.
    it's now 3 seperate input lines.

--- src/js/api/Listings/post.js
+++ src/js/api/Listings/post.js
@@ -5,19 +5,26 @@ export async function createListing() {
   const titleInput = document.getElementById("listingTitle");
   const descriptionInput = document.getElementById("listingDescription");
 
-  const mediaUrlsInput = document.getElementById("listingMediaUrls");
+  const mediaUrl1Input = document.getElementById("mediaUrl1");
+  const mediaUrl2Input = document.getElementById("mediaUrl2");
+  const mediaUrl3Input = document.getElementById("mediaUrl3");
+
   const endsAtInput = document.getElementById("listingEndsAt");
 
   const title = titleInput.value;
   const description = descriptionInput.value;
 
-  const mediaUrls = mediaUrlsInput.value.split('\n').map(url => ({ url: url.trim(), alt: "Auction item image" }));
+  const mediaUrls = [
+    { url: mediaUrl1Input.value.trim(), alt: "Auction main item image" },
+    { url: mediaUrl2Input.value.trim(), alt: "Auction item image 2" },
+    { url: mediaUrl3Input.value.trim(), alt: "Auction item image 3" }
+  ];
+
   const endsAt = endsAtInput.value;
 
   const listingData = {
     title: title,
     description: description,
-   
     media: mediaUrls,
     endsAt: endsAt,
   };
@@ -47,20 +54,18 @@ export async function createListing() {
       console.log("Listing created successfully:", data);
       titleInput.value = "";
       descriptionInput.value = "";
-     
-      mediaUrlsInput.value = "";
+      mediaUrl1Input.value = "";
+      mediaUrl2Input.value = "";
+      mediaUrl3Input.value = "";
       endsAtInput.value = "";
       location.reload();
     })
     .catch(error => {
-      let errorMessage = "Unexpected Error. Please fill out the required feilds and try again.";
-    
+      let errorMessage = "Unexpected Error. Please fill out the required feilds and try again.";    
       
       const createListingErrorsDiv = document.getElementById('createListingErrors');
       if (createListingErrorsDiv) {
         createListingErrorsDiv.innerHTML = errorMessage;
       }
     });
-    
-
 }
